d492e7337a017c1187e636706a0c26df38ae55d6
Mitul Mungase <120619177+Mitul30M@users.noreply.github.com>
2026-07-27 10:12:17 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,24 @@
+from flask import Flask, request, jsonify
+import pandas as pd
+import joblib
+
+app = Flask(__name__)
+
+model = joblib.load("model.pkl")
+
+@app.route("/predict", methods=["POST"])
+def predict():
+
+    data = request.get_json()
+
+    df = pd.DataFrame([data])
+
+    prediction = model.predict(df)
+
+    return jsonify({
+
+        "prediction": int(prediction[0])
+
+    })
+
+app.run(debug=True)
